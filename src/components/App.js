c336d00d822446de74dabeb2640edc1f86f279c6
ngani faith <nganifaith@gmail.com>
2021-07-27 15:21:15 +0100

    Add display section

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ButtonPanel from './ButtonPanel';
+import Display from './Display';
 
 function App() {
   return (
     <div className="App">
       <div className="calc-wrapper">
+        <Display />
         <ButtonPanel />
       </div>
     </div>
