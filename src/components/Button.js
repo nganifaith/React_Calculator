86d9c85dac926193771988288d8141b9132cb945
ngani faith <nganifaith@gmail.com>
2021-07-27 14:45:24 +0100

    Set up based components

---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ symbol }) => <div className="button-wrapper">{symbol}</div>;
+
+export default Button;
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,9 @@
+import Button from './Button';
+
+const ButtonPanel = () => (
+  <div className="button-wrapper">
+    <Button />
+  </div>
+);
+
+export default ButtonPanel;
