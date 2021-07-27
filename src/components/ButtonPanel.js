66bcaf5febbce74f699437aa31638c7e21e69525
ngani faith <nganifaith@gmail.com>
2021-07-27 16:16:23 +0100

    Change logic to create button layout

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,40 +1,22 @@
 import Button from './Button';
 
-const btnRows = {
-  row_1: ['AC', '+/-', '%', '/'],
-  row_2: ['7', '8', '9', 'X'],
-  row_3: ['4', '5', '6', '-'],
-  row_4: ['1', '2', '3', '+'],
-  row_5: ['0', '.', '='],
-};
+const btnRows = [
+  ['AC', '+/-', '%', '/'],
+  ['7', '8', '9', 'X'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '='],
+];
 
 const ButtonPanel = () => (
   <div>
-    <div className="row">
-      {btnRows.row_1.map((symbol) => (
-        <Button key={symbol} symbol={symbol} />
-      ))}
-    </div>
-    <div className="row">
-      {btnRows.row_2.map((symbol) => (
-        <Button key={symbol} symbol={symbol} />
-      ))}
-    </div>
-    <div className="row">
-      {btnRows.row_3.map((symbol) => (
-        <Button key={symbol} symbol={symbol} />
-      ))}
-    </div>
-    <div className="row">
-      {btnRows.row_4.map((symbol) => (
-        <Button key={symbol} symbol={symbol} />
-      ))}
-    </div>
-    <div className="row">
-      {btnRows.row_5.map((symbol) => (
-        <Button key={symbol} symbol={symbol} />
-      ))}
-    </div>
+    {btnRows.map((row) => (
+      <div key={row[0]} className="row">
+        {row.map((symbol) => (
+          <Button key={symbol} symbol={symbol} />
+        ))}
+      </div>
+    ))}
   </div>
 );
 
