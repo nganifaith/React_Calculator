1a1d9b4e10cf9785da10274432deb99f21b7ea43
ngani faith <nganifaith@gmail.com>
2021-07-28 23:21:21 +0100

    update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Display />
-      <ButtonPanel onCalculate={Calculate} />
+      <ButtonPanel onCal={Calculate} />
     </div>
   );
 }
