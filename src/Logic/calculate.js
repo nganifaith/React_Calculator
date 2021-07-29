ab8b478d14302fcc2e5ea80dffdd488ce3a378d8
ngani faith <nganifaith@gmail.com>
2021-07-29 09:08:40 +0100

    Update logic for . buttonName

--- src/Logic/calculate.js
+++ src/Logic/calculate.js
@@ -11,8 +11,13 @@ const Calculate = (data, buttonName) => {
   } else if (buttonName === '+/-') {
     total = new Big(total).times(-1);
     next = new Big(next).times(-1);
-  } else if (Number.isInteger(parseInt(buttonName, 10)) || buttonName === '.') {
+  } else if (Number.isInteger(parseInt(buttonName, 10))) {
     next = `${next || ''}${buttonName}`;
+  } else if (buttonName === '.') {
+    next = `${next || ''}`;
+    if (!next.includes('.')) {
+      next = `{$next || ''}${buttonName}`;
+    }
   } else if (buttonName === '=') {
     total = Operate(total, next, operation);
     next = null;
