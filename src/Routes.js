73467c092decc4e898e173d5e49ce9d76e06df5c
ngani faith <nganifaith@gmail.com>
2021-08-04 23:04:45 +0100

    Add header component

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Header from './components/Header';
 
 const Routes = () => (
   <BrowserRouter>
+    <Header />
     <Switch>
       <Route path="/" component={Calculator} />
     </Switch>
